3005595c753e11fd02ebe387fab5bc4e50613af3
Simon Redding <s1m0n.r3dd1ng@gmail.com>
2017-03-07 17:30:35 +0000

    FLat rate distance prcing added

--- rydz.py
+++ rydz.py
@@ -1,8 +1,22 @@
 #!/usr/local/bin/python3
 
 class Address:
-  def __init__(self, postcode):
+  def __init__(self, number=None, street=None, town=None, postcode=None, country=None):
+    self.number=number
+    self.street=street
+    self.town=town
     self.postcode=postcode
+    self.country=country
+
+  def __eq__(self, other):
+    if isinstance(other, self.__class__):
+      return self.__dict__ == other.__dict__
+    return NotImplemented
+
+  def __ne__(self, other):
+    if isinstance(other, self.__class__):
+      return self.__dict__ != other.__dict__
+    return NotImplemented
 
 
 class UKAddress(Address):
@@ -10,9 +24,26 @@ class UKAddress(Address):
     return self.postcode.split(' ')[0]
 
 
-class PostcodeRatebook:
+class USAddress(Address):
+  def postcode_area(self):
+    return self.postcode[:3]
+
+
+class PostcodeRateBook:
   def __init__(self, postcode_map):
     self.postcode_map=postcode_map
 
   def price(self, origin, destination):
     return self.postcode_map[origin.postcode_area()][destination.postcode_area()]
+
+class DistanceSource:
+  def distance(self, origin, destination):
+    pass
+
+class FlatRateDistanceRateBook:
+  def __init__(self, distance_source, rate_pence_per_mile):
+    self.rate=rate_pence_per_mile
+    self.distance_source=distance_source
+
+  def price(self, origin, destination):
+    return self.rate * self.distance_source.distance(origin, destination)
--- test_rydz.py
+++ test_rydz.py
@@ -1,15 +1,31 @@
 #!/usr/local/bin/python3
 from unittest import TestCase,main
-from rydz import PostcodeRatebook, UKAddress
+from unittest.mock import MagicMock
+from rydz import PostcodeRateBook, Address, UKAddress, USAddress,\
+  DistanceSource, FlatRateDistanceRateBook
 
 class TestAddress(TestCase):
-  def test_postcode_area(self):
+  def test_fully_populated(self):
+    a=Address(number=56, street='King Edward Road', town='Teddington',
+              postcode='TW11 2BC', country='UK')
+    self.assertEqual(56, a.number)
+    self.assertEqual('King Edward Road', a.street)
+    self.assertEqual('Teddington', a.town)
+    self.assertEqual('TW11 2BC', a.postcode)
+    self.assertEqual('UK', a.country)
+
+  def test_postcode_area_UK(self):
     a=UKAddress(postcode='RM14 1PX')
     self.assertEqual('RM14', a.postcode_area())
 
+  def test_postcode_area_US(self):
+    a=USAddress(postcode='90210')
+    self.assertEqual('902', a.postcode_area())
+
+
 class TestPostcodePricing(TestCase):
   def setUp(self):
-    self.ratebook = PostcodeRatebook({'TW11':{'NW1':22.5, 'RM14':65.25},
+    self.ratebook = PostcodeRateBook({'TW11':{'NW1':22.5, 'RM14':65.25},
                                      'NW1': {'RM14':52.5, 'TW11':23.25},
                                      'RM14':{'NW1':62.5, 'TW11':63.25}})
 
@@ -29,6 +45,16 @@ class TestPostcodePricing(TestCase):
       self.assertEqual(63.25, self.ratebook.price(UKAddress(postcode='RM14 1AB'),
                                                   UKAddress(postcode='TW10 2CD')))
 
+
+class TestDistancePricing(TestCase):
+  def test_flat_rate(self):
+    distance_source=MagicMock(DistanceSource)
+    distance_source.distance=MagicMock(return_value=100)
+    ratebook=FlatRateDistanceRateBook(distance_source, 0.5)
+    self.assertEqual(50, ratebook.price(Address(postcode='NW1 1AB'),
+                                        Address(postcode='RM14 2CD')))
+    distance_source.distance.assert_called_with(Address(postcode='NW1 1AB'),
+                                              Address(postcode='RM14 2CD')) 
 
 
 if __name__=='__main__':
